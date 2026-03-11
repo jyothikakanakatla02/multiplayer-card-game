e1b3ec5a1a7f3c4fe535584b19089770be0a565a
Jyothika <your-github-email@example.com>
2026-03-11 14:48:11 +0530

    Sprint 4: Implement start game logic and lobby state validation

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_room_view,join_room,room_state,leave_room
+from .views import create_room_view,join_room,room_state,leave_room,start_game
 urlpatterns = [
     path("create-room/", create_room_view),
     path("join-room/", join_room, name="join-room"),
     path("room-state/<str:room_id>/", room_state),
     path("leave-room/", leave_room),
+    path("start-game/", start_game),
 ]
 
